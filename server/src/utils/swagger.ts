1d6f3ad716a6d42c3ea4f63f5bc4407ab27cb40d
hwansu-k <kuma2301@naver.com>
2021-12-28 10:25:40 +0900

    ✨ Add api docs. init

---
+++ server/src/utils/swagger.ts
@@ -0,0 +1,21 @@
+export * as swaggerUi from 'swagger-ui-express';
+import swaggereJsdoc from 'swagger-jsdoc';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const options = {
+  swaggerDefinition: {
+    swagger: '2.0',
+    info: {
+      title: 'Clean Chat API',
+      version: '1.0.0',
+      description: '',
+    },
+    host: process.env.API_HOST,
+    schemes: ['https', 'http'],
+  },
+  apis: ['./src/models/*.ts', './src/routes/*.ts', './src/utils/*.yaml'],
+};
+
+export const specs = swaggereJsdoc(options);
