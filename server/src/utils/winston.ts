bcf0f1c280a113d90a2005da82ab76140fff7c3c
hwansu-k <kuma2301@naver.com>
2022-01-09 13:49:01 +0900

    :bug: the file is missing

---
+++ server/src/utils/winston.ts
@@ -0,0 +1,60 @@
+import winston from 'winston';
+import winstonDaily from 'winston-daily-rotate-file';
+
+const logDir = 'logs';
+const { combine, timestamp, printf } = winston.format;
+
+const logFormat = printf((info) => {
+  return `${info.timestamp} ${info.level}: ${info.message}`;
+});
+
+const logger = winston.createLogger({
+  format: combine(
+    timestamp({
+      format: 'YYYY-MM-DD HH:mm:ss',
+    }),
+    logFormat
+  ),
+  transports: [
+    new winstonDaily({
+      level: 'info',
+      datePattern: 'YYYY-MM-DD',
+      dirname: logDir,
+      filename: `%DATE%.log`,
+      maxFiles: 30,
+      zippedArchive: true,
+    }),
+    new winstonDaily({
+      level: 'warn',
+      datePattern: 'YYYY-MM-DD',
+      dirname: logDir + '/warn',
+      filename: `%DATE%.warn.log`,
+      maxFiles: 30,
+      zippedArchive: true,
+    }),
+    new winstonDaily({
+      level: 'error',
+      datePattern: 'YYYY-MM-DD',
+      dirname: logDir + '/error',
+      filename: `%DATE%.error.log`,
+      maxFiles: 30,
+      zippedArchive: true,
+    }),
+  ],
+});
+
+if (process.env.NODE_ENV !== 'production') {
+  logger.add(
+    new winston.transports.Console({
+      format: winston.format.combine(winston.format.colorize(), winston.format.simple()),
+    })
+  );
+}
+
+const stream = {
+  write: (message: string) => {
+    logger.info(message);
+  },
+};
+
+export { logger, stream };
