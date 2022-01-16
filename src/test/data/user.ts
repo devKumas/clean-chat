8e46a61e99bc9f5567351cba7fd0a3ab06ab57ce
hwansu-k <kuma2301@naver.com>
2022-01-17 00:00:22 +0900

    :white_check_mark: Simplify mock data

--- src/test/data/user.ts
+++ src/test/data/user.ts
@@ -1,8 +1,60 @@
 export default {
   id: 1,
   email: 'admin@kumas.dev',
-  password: 'password',
   name: '홍길동',
   gender: 'M',
   imagePath: '',
+};
+
+export const getUserById = {
+  getUser: {
+    id: 1,
+    email: 'admin@kumas.dev',
+    name: '홍길동',
+    gender: 'M',
+    imagePath: '',
+  },
+};
+
+export const getUserByEmail = {
+  getUser: {
+    id: 1,
+    email: 'admin@kumas.dev',
+    name: '홍길동',
+    gender: 'M',
+    imagePath: '',
+  },
+};
+
+export const createUser = {
+  exUser: {
+    id: 1,
+  },
+
+  newUser: {
+    id: 1,
+    email: 'admin@kumas.dev',
+    name: '홍길동',
+    gender: 'M',
+    imagePath: '',
+  },
+};
+
+export const updateUser = {
+  exUser: {
+    id: 1,
+    email: 'admin@kumas.dev',
+    name: '홍길동',
+    gender: 'M',
+    imagePath: '',
+  },
+  changedUser: {
+    exUser: {
+      id: 1,
+      email: 'admin@kumas.dev',
+      name: '홍길동',
+      gender: 'M',
+      imagePath: '',
+    },
+  },
 };
